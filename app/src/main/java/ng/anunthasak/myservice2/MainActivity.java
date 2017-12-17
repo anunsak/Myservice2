aada0af943064cc57a77d509fc88911dee22f5a4
nasterUNG <phronbutr@gmail.com>
2017-12-17 09:39:47 +0700

    Design wallpaper Ok

--- app/src/main/java/ng/anunthasak/myservice2/MainActivity.java
+++ app/src/main/java/ng/anunthasak/myservice2/MainActivity.java
@@ -3,11 +3,23 @@ package ng.anunthasak.myservice2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ng.anunthasak.myservice2.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+
+
+        }
+
+
+    } // main Method
+}  //Main class
